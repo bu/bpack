7c3a1f28263d877a5a47ab454e77ad4770464e01
Buwei Chiu <bu@bu.idv.tw>
2020-06-24 02:33:36 +0800

    Request fix issue that cannot extract value from user input

--- bpack/Request.php
+++ bpack/Request.php
@@ -3,38 +3,24 @@ namespace bPack;
 
 // TODO: PSR-7 should apply here
 class Request implements Protocol\Request {
-
     public function get(string $key, $default_value = null, $options = array() ) {
-        return $this->extractValue("GET", $default_value, $options);
+        return $this->extractValue(INPUT_GET, $key, $default_value, $options);
     }
 
     public function post(string $key, $default_value = null, $options = array() ) {
-        return $this->extractValue("POST", $default_value, $options);
+        return $this->extractValue(INPUT_POST, $key, $default_value, $options);
     }
 
     public function cookie(string $key, $default_value = null, $options = array() ) {
-        return $this->extractValue("COOKIE", $key, $default_value, $options);
+        return $this->extractValue(INPUT_COOKIE, $key, $default_value, $options);
     }
 
-    protected function extractValue(string $method, string $key, $default_value = null, $options = null) {
+    protected function extractValue(int $method, string $key, $default_value = null, $options = null) {
 
         $options ??= [];
         $options["filter"] ??= FILTER_SANITIZE_STRING;
-        $options["flag"] ??= [];
 
-        // get the value
-        if( !isset(${"_" . $method}[$key]) ) {
-            return $default_value;
-        }
-
-        $value = ${"_" . $method}[$key];
-
-        // if key is an array
-        if( is_array($value) ) {
-            $options["flag"][] = FILTER_REQUIRE_ARRAY;
-        }
-
-        return filter_var($value, $options["filter"], $options["flag"]);
+        return filter_input($method, $key, $options["filter"]) ?? $default_value;
     }
 
 }
